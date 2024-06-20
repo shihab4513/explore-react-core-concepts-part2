b66fb500424033f9a56ab0596748a2944e6501d7
shihab4513 <shihab4513@gmail.com>
2024-06-20 21:34:17 +0600

    different ways to handle events discussed in this commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+
+  function handleClick() {
+    alert('button clicked');
+  }
+  const handleClick2 = () => {
+    alert('button 2 clicked');
+  }
+  const addToFive = (num) => {
+
+    alert(num + 5);
+
+  }
+
 
   return (
     <>
-    
-      <h3>Vite + React</h3>
-     
+
+
+      <h3>React</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click2</button>
+      <button onClick={() => {
+        { alert('third click ') }
+      }}>Click3</button>
+      {/* to call function with parameter in jsx you have to wrap that function in another another anonymous function else it wont work */}
+      <button onClick={() => { addToFive(10) }}>Four</button>
+
     </>
   )
 }
